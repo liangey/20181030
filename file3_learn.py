a6f9c2ccb3bcc3824b3e5add4a6a12fcac5302e1
liangey <291406980@qq.com>
2018-12-06 14:23:43 +0800

    add new files

--- file3_learn.py
+++ file3_learn.py
@@ -6,4 +6,21 @@
 #@File : file3_learn.py
 #@Software : PyCharm
 
-##file3
+
+f1=open('D:\\sea\\PycharmProjects\\20181030\\test.txt','r+')
+f1.write('qd')
+f1.close()
+
+#print(f1.readlines())
+f2=open('D:\\sea\\PycharmProjects\\20181030\\test.txt','r')
+print(f2.readlines())
+f2.close()
+
+f3=open('D:\\sea\\PycharmProjects\\20181030\\test.txt','w+')
+for line in (i**2 for i in range(1,11)):
+    f3.write(str(line)+'\n')
+f3.flush()
+f3.close()
+print(f3)
+
+

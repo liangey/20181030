b9f6baf0ff9f8832ca803fa00d870f7d69ad8139
liangey <291406980@qq.com>
2018-12-17 20:37:08 +0800

    add new files

--- func_learn.py
+++ func_learn.py
@@ -30,3 +30,25 @@ num2=int(input("请输入另外一个整数:"))
 rr=summ(num1,num2)
 
 print(rr)
+
+
+
+def shoppingcart():
+    balance=10000
+    shopping_cart=[
+        ('mac',9000),
+        ('kindle',800),
+        ('tesla',100000),
+        ('python_book',120),
+    ]
+
+    print("shoppingcart".center(50,'*'))
+    for i,v in enumerate(shopping_cart):
+        print('\033[35;1m %s:  %s \033[0m'%(i,v))
+
+    expense=0
+    for i in shopping_cart:
+        expense += i[1]
+    print('\n\033[32;1m您的余额为 %s \033[0m' % (balance - expense))
+
+shoppingcart()
